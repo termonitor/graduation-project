c2ee87306302417664815cc52ced422d5c7203c5
termonitor <pxm2525@163.com>
2016-03-24 11:00:37 +0800

    startup.sh test pass

--- startup.sh
+++ startup.sh
@@ -2,12 +2,16 @@
 if [ ! -d /sys/class/gpio/gpio76 ]; then
 	echo 76 > /sys/class/gpio/export
 fi
-sw_value = cat /sys/class/gpio/gpio76/value;
-while [$sw_value -eq 0]
+while [ "1" = "1" ];
 do
-	#wait 
-done
-if [sw_value -eq 1]; then
-	#执行程序
-	/mnt/SD/main
-fi
+	sw_value=`cat /sys/class/gpio/gpio76/value`;
+	while [ $sw_value -eq 0 ];
+	do
+		sw_value=`cat /sys/class/gpio/gpio76/value`;
+		#wait
+	done;
+	if [ $sw_value -eq 1 ]; then
+		#执行程序
+		/mnt/SD/test/main
+	fi
+done;
