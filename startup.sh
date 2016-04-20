712022020da915de5b9841162d35b9c3221dd53d
termonitor <pxm2525@163.com>
2016-04-20 15:22:03 +0800

    add some change and not test

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@ if [ ! -d /sys/class/gpio/gpio76 ]; then
 	echo 76 > /sys/class/gpio/export
 fi
 #simple-agent的注册
+./simple-agent &
 #my_knn模块的插入
+insmod my_knn.ko
 while [ "1" = "1" ];
 do
 	sw_value=`cat /sys/class/gpio/gpio76/value`;
